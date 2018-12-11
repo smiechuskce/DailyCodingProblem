bf0894b6ba31dbe6dd4f75ec25c6fba506a53cd1
Kamil Śmieszek <kamil.smieszek@gmail.com>
2018-12-11 08:56:15 +0100

    Added correct onepass solution based on set.

--- 1-SumComparation.py
+++ 1-SumComparation.py
@@ -1,10 +1,17 @@
+# DailyCodingProblem #1
+#
 # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 #
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 # 
 # Bonus: Can you do this in one pass?
 
+# Solution 1: Naive - Compare every single number with another and find if the sum is k
+
 def CompareSumWithK(numbers, k):
+
+    if len(numbers) < 2:
+        return False
 
     start = 0
 
@@ -15,17 +22,29 @@ def CompareSumWithK(numbers, k):
         start = start + 1
     return False
 
-def CompareSumWithKInOnePass(numbers, k, start):
-    
-    if len(numbers) == 2 and numbers[0] + numbers[1] == k:
-        return True
-    elif len(numbers) < 2:
+# Solution 2: One pass - Find the differences between numbers and k, store them in a set and after that search if number exists in this set
+
+def CompareSumWithKInOnePass(numbers, k):
+
+    if len(numbers) < 2:
         return False
-    else:
-        return CompareSumWithKInOnePass(numbers[start:], k, start)
 
-numbers = [10, 15, 3, 7]
-k = 25
+    substractResults = set()
 
-result = CompareSumWithKInOnePass(numbers, k, 1)
+    for number in numbers:
+        substractResults.add(k - number)
+
+    for i in range(0, len(numbers)):
+        if numbers[i] in substractResults:
+            return True
+
+    return False
+
+numbers = [1, 15, 3, 7]
+k = 4
+
+result = "For loop version has given a: " + str(CompareSumWithK(numbers, k))
+print result
+
+result = "One pass version has given a: " + str(CompareSumWithKInOnePass(numbers, k))
 print result
