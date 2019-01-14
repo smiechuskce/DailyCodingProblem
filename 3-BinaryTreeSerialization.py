b7353045898c3e5261960755773d5b187550ab15
Kamil Smieszek <kamil.smieszek@transteka.pl>
2019-01-14 23:08:27 +0100

    Added preorder tree traversal.

--- 3-BinaryTreeSerialization.py
+++ 3-BinaryTreeSerialization.py
@@ -14,12 +14,24 @@
 # node = Node('root', Node('left', Node('left.left')), Node('right'))
 # assert deserialize(serialize(node)).left.left.val == 'left.left'
 
+#def serialize(root):
+
+
+def traverse_preorder(root):
+    if root == None:
+        return
+    
+    print root.val
+    traverse_preorder(root.left)
+    traverse_preorder(root.right)
+
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-node = Node('root', Node('left', Node('left.left')), Node('right'))
+node = Node('root', Node('left', Node('left.left'), Node('left.right')), Node('right'))
+traverse_preorder(node)
 
-assert deserialize(serialize(node)).left.left.val == 'left.left'
+# assert deserialize(serialize(node)).left.left.val == 'left.left'
